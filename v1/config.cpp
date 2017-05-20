9020e9f4f8dca10915ed76b2951deaf45724432c
10Dozen <steel.mjr@gmail.com>
2017-05-20 14:50:52 +0300

    Create config.cpp

---
+++ v1/config.cpp
@@ -0,0 +1,20 @@
+class CfgPatches
+{
+	class dzn_IWB
+	{		
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1.0;
+		requiredAddons[] = {"CBA_MAIN"};
+		version = "V1";
+		author[] = {"10Dozen"};
+	};
+};
+
+class Extended_PostInit_EventHandlers
+{
+	class dzn_IWB
+	{
+		serverInit = "call ('\dzn_IWB\init.sqf' call SLX_XEH_COMPILE)";
+	};
+};
